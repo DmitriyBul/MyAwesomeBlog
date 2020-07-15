d5420c42e63239ad2a9722aaa9b33ea346401448
Dmitriy Bulygin <128dmitriy128@gmail.com>
2020-07-15 10:31:06 +0500

    Added post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='post_images/')
+    text = models.TextField(max_length=300)
+    date = models.DateField()
